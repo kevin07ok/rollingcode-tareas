52cae436680c5c91830586986039905a2243cbcc
kevin guzman <guzmank685@gmail.com>
2021-07-30 12:50:41 -0300

    se modificaron cambios y se creo un nuevo componente

---
+++ src/components/Titulo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titulo = () => {
+    return (
+        <div>
+            <h1 className='text-center text-light display-4 my-4'>Mi Lista De Tareas</h1>
+        </div>
+    );
+};
+
+export default Titulo;
+
