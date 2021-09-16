7b328c5f3804924eb3da41c444208a8c7c15978f
kevin guzman <guzmank685@gmail.com>
2021-09-16 16:52:36 -0300

    se agregaron los states al componente formTareas

--- src/components/FormTareas.js
+++ src/components/FormTareas.js
@@ -1,30 +1,55 @@
-import React from "react";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Container, Form, Button, FormGroup } from "react-bootstrap";
 
 const FormTareas = () => {
+  const [nombreTarea, setNombreTarea] = useState("");
+  const [categoria, setCategoria] = useState("");
+
+
+  const handleSubmit = (e) =>{
+      e.preventDefault();
+      // validar los datos //
+
+      // enviar el producto a la api //
+      
+  }
+
   return (
     <Fragment>
-        <Container className="my-4">
-        <Form>
-        <h1 className="my-4 text-center">Mis Nuevas Tareas</h1>
-        <FormGroup className='my-2'>Agregar *</FormGroup>
+      <Container className="my-4">
+        <Form onSubmit={handleSubmit}>
+          <h1 className="my-4 text-center">Mis Nuevas Tareas</h1>
+          <FormGroup className="my-2">Agregar *</FormGroup>
           <Form.Control
             type="text"
             placeholder=" Tarea"
+            onChange={(e) => setNombreTarea(e.target.value)}
           ></Form.Control>
-          <div className='text-center my-4'>
-          <h3>Estado</h3>
-          <Form.Check inline type="radio" label="Completa" name='estado'></Form.Check>
-          <Form.Check inline type="radio" label="Incompleta" name='estado'></Form.Check>
+          <div className="text-center my-4">
+            <h3>Categoria</h3>
+            <Form.Check
+              inline
+              type="radio"
+              label="Completa"
+              name="estado"
+              value="completa"
+              onChange={(e) => setCategoria(e.target.value)}
+            ></Form.Check>
+            <Form.Check
+              inline
+              type="radio"
+              label="Incompleta"
+              name="estado"
+              value="incompleta"
+              onChange={(e) => setCategoria(e.target.value)}
+            ></Form.Check>
           </div>
-          </Form>
+        </Form>
         <Button variant="danger" type="submit" className="w-100 my-3">
           Agregar Tarea
         </Button>
-        </Container>
-      </Fragment>
-      
+      </Container>
+    </Fragment>
   );
 };
 
